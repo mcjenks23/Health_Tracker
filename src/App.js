a1f4e53ee5345ffbd0fe1a880b06b49f93fac08c
Matthew Jenkins <mcjenks23@gmail.com>
2019-11-11 13:57:06 -0700

    Setup and Survey Setup

--- src/App.js
+++ src/App.js
@@ -2,178 +2,24 @@ import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import FavoriteIcon from "@material-ui/icons/Favorite";
 import Drawer from "@material-ui/core/Drawer";
-import { Link } from "react-router-dom";
-import { auth, db } from "./firebase";
+import { auth } from "./firebase";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
+import { Link } from "react-router-dom";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-
-export function SignIn(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      //do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const handleSignIn = () => {
-    console.log(email);
-    console.log(password);
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        //navigate to new screen
-      })
-      .catch(error => {
-        alert(error.message);
-        console.log(error);
-        //display error message
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography color="inherit" variant="h6">
-            Sign In
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "400px", marginTop: 30, padding: "40px" }}>
-          <TextField
-            fullWidth={true}
-            placeholder={"email"}
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            fullWidth={true}
-            placeholder="password"
-            type={"password"}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-            style={{ marginTop: 20 }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginTop: "30px",
-              alignItems: "center"
-            }}
-          >
-            <div>
-              Don't have an account? <Link to="/signup">Sign up!</Link>
-            </div>
-
-            <Button color="primary" variant="contained" onClick={handleSignIn}>
-              Sign In
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
-
-export function SignUp(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      //do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {})
-      .catch(error => {
-        console.log("error");
-        alert(error.message);
-        //display error message
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography color="inherit" variant="h6">
-            Sign Up
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "400px", marginTop: 30, padding: "40px" }}>
-          <TextField
-            fullWidth={true}
-            placeholder="email"
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            fullWidth={true}
-            placeholder="password"
-            type={"password"}
-            style={{ marginTop: 20 }}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginTop: "30px",
-              alignItems: "center"
-            }}
-          >
-            <div>
-              Already have an account? <Link to="/">Sign In!</Link>
-            </div>
-
-            <Button color="primary" variant="contained" onClick={handleSignUp}>
-              Sign Up
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
+import { Route } from "react-router";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import DissatisfiedIcon from "@material-ui/icons/SentimentDissatisfied";
+import SatisfiedIcon from "@material-ui/icons/SentimentSatisfied";
+import VeryDissatisfiedIcon from "@material-ui/icons/SentimentVeryDissatisfied";
+import VerySatisfiedIcon from "@material-ui/icons/SentimentVerySatisfied";
+import { Line } from "react-chartjs-2";
 
 export function App(props) {
   const [drawer_open, setDrawerOpen] = useState(false);
@@ -190,31 +36,6 @@ export function App(props) {
 
     return unsubscribe;
   }, [props.history]);
-
-  useEffect(() => {
-    let unsubscribe;
-
-    if (user) {
-      unsubscribe = () => {
-        db.collection("users")
-          .doc(user.uid)
-          .collection("tasks")
-          .onSnapshot(snapshot => {
-            const user_tasks = snapshot.docs.map(qs => {
-              const task = {
-                id: qs.id,
-                text: qs.data().text,
-                complete: qs.data().complete
-              };
-              return task;
-            });
-            setTasks(user_tasks);
-          });
-      };
-    }
-
-    return unsubscribe;
-  }, [user]);
 
   const handleMenuOpen = () => {
     setDrawerOpen(true);
@@ -239,26 +60,6 @@ export function App(props) {
     return <div />;
   }
 
-  const sideList = side => (
-    <div>
-      <List>
-        {["Te Amo"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {
-                <IconButton>
-                  <FavoriteIcon />
-                </IconButton>
-              }
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </div>
-  );
-
   return (
     <div>
       <AppBar position="static">
@@ -271,7 +72,7 @@ export function App(props) {
             variant="h6"
             style={{ marginLeft: 15, flexGrow: 1 }}
           >
-            News
+            Health Tracker
           </Typography>
           <Typography color="inherit" style={{ marginRight: 30 }}>
             Hi! {user.email}
@@ -283,8 +84,149 @@ export function App(props) {
         </Toolbar>
       </AppBar>
       <Drawer open={drawer_open} onClose={handleCloseDrawer}>
-        {sideList("left")}
+        <List>
+          <ListItem
+            button
+            to="/app/"
+            component={Link}
+            onClick={handleCloseDrawer}
+          >
+            <ListItemText primary="Take Survey" />
+          </ListItem>
+          <ListItem
+            button
+            to="/app/charts/"
+            component={Link}
+            onClick={handleCloseDrawer}
+          >
+            <ListItemText primary="Chart" />
+          </ListItem>
+        </List>
       </Drawer>
+      <Route exact path="/app" component={Survey} />
+      <Route path="/app/charts" component={Charts} />
+    </div>
+  );
+}
+
+function Survey(props) {
+  const [radioValue, setRadioValue] = useState(2);
+  const [sleep, setSleep] = useState(8);
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Paper style={{ padding: 30, width: 400, marginTop: 30 }}>
+        <Typography>How many hours did you sleep last night?</Typography>
+        <TextField
+          fullWidth
+          value={sleep}
+          onChange={e => {
+            setSleep(e.target.value);
+          }}
+        />
+        <Typography style={{ marginTop: 20 }}>
+          How happy do you feel today?
+        </Typography>
+        <div style={{ marginTop: 10 }}>
+          <Radio
+            icon={<VerySatisfiedIcon />}
+            checkedIcon={<VerySatisfiedIcon />}
+            value={1}
+            checked={radioValue === 1}
+            onChange={() => {
+              setRadioValue(1);
+            }}
+          />
+          <Radio
+            icon={<SatisfiedIcon />}
+            checkedIcon={<SatisfiedIcon />}
+            value={2}
+            checked={radioValue === 2}
+            onChange={() => {
+              setRadioValue(2);
+            }}
+          />
+          <Radio
+            icon={<DissatisfiedIcon />}
+            checkedIcon={<DissatisfiedIcon />}
+            value={3}
+            checked={radioValue === 3}
+            onChange={() => {
+              setRadioValue(3);
+            }}
+          />
+          <Radio
+            icon={<VeryDissatisfiedIcon />}
+            checkedIcon={<VeryDissatisfiedIcon />}
+            value={4}
+            checked={radioValue === 4}
+            onChange={() => {
+              setRadioValue(4);
+            }}
+          />
+        </div>
+        <div>
+          <Button variant="contained" color="primary" style={{ marginTop: 20 }}>
+            Save
+          </Button>
+        </div>
+      </Paper>
+    </div>
+  );
+}
+
+function Charts(props) {
+  const data = {
+    datasets: [
+      {
+        label: "Temperature",
+        data: [
+          { x: "2019-01-01", y: 70 },
+          { x: "2019-01-05", y: 60 },
+          { x: "2019-01-10", y: 53 }
+        ],
+        backgroundColor: "transparent",
+        borderColor: "blue",
+        yAxesID: "A"
+      },
+      {
+        label: "Sleep",
+        data: [
+          { x: "2019-01-01", y: 6 },
+          { x: "2019-01-05", y: 7 },
+          { x: "2019-01-10", y: 6 }
+        ],
+        backgroundColor: "transparent",
+        borderColor: "green",
+        yAxesID: "B"
+      },
+      {
+        label: "Happiness",
+        data: [
+          { x: "2019-01-01", y: 7 },
+          { x: "2019-01-05", y: 5 },
+          { x: "2019-01-10", y: 3 }
+        ],
+        backgroundColor: "transparent",
+        borderColor: "red",
+        yAxesID: "B"
+      }
+    ]
+  };
+
+  const options = {
+    scales: {
+      yAxes: [{ id: "A", position: "left" }, { id: "B", position: "right" }],
+      xAxes: [{ type: "time", time: { displayFormats: { hour: "MMM D" } } }]
+    }
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Paper style={{ width: 600, marginTop: 30, padding: 30 }}>
+        <Typography variant="h6" style={{ marginBottom: 30 }}>
+          Health Stats over Time
+        </Typography>
+        <Line data={data} options={options} />
+      </Paper>
     </div>
   );
 }
